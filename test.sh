b55fd4a695263dfb639c213313122675861f202d
Mark Polyak <markpolyak@gmail.com>
2020-02-17 09:47:01 +0300

    Update shUnit2 from 2.1.6 to 2.1.7 and add new tests

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 unameOut="$(uname -s)"
 case "${unameOut}" in
@@ -16,35 +16,133 @@ else
     MD5_SHELL="md5sum "
 fi
 
+# $1 - test suit
+# $2 - TASKID
+# $3 - field ID
+readData()
+{
+    if [[ $1 -eq 1 ]]
+    then
+        grep "^$2\s" shunit2-2.1.7/full.txt | cut -f $3
+    elif [[ $1 -eq 2 ]]
+    then
+        grep "^$2\s" shunit2-2.1.7/notsofull.txt | cut -f $3
+    else
+        "Error!"
+    fi
+}
+
+oneTimeSetUp()
+{
+    cp dns-tunneling.log dns-tunneling.log.bak
+}
+
+oneTimeTearDown()
+{
+    mv dns-tunneling.log.bak dns-tunneling.log
+}
+
 setUp()
 {
     rm results.*
+    TASKID=-1
+    VAR_1=-1
+    VAR_2=-1
 }
 
-testLab1()
+test_TASKID()
 {
-    # set correct locale
-    LC_ALL=C
-    # execute student's solution
-    bash lab1.sh
+    source lab1.sh
+    echo "TASKID is "$TASKID
+    assertNotEquals "Did you forget to set the TASKID variable?" -1 $TASKID
+    assertTrue "TASKID is too small" "[ $TASKID -gt 0 ]"
+    assertTrue "TASKID is too large" "[ $TASKID -lt 20 ]"
+}
+
+test_VAR_1_case1()
+{
+    setUp_case1
+    source lab1.sh
+    expected=$(readData 1 $TASKID 2)
+    echo "VAR_1 is "$VAR_1
+    assertNotEquals "Did you forget to set the VAR_1 variable?" -1 $VAR_1
+    assertEquals "Wrong value of VAR_1!" $expected $VAR_1
+}
+
+test_VAR_2_case1()
+{
+    setUp_case1
+    source lab1.sh
+    expected=$(readData 1 $TASKID 3)
+    echo "VAR_2 is "$VAR_2
+    assertNotEquals "Did you forget to set the VAR_2 variable?" -1 $VAR_2
+    assertEquals "Wrong value of VAR_2!" $expected $VAR_2
+}
+
+test_results_file_case1()
+{
+    setUp_case1
+    source lab1.sh
+    touch results.txt
+    # assertTrue "script was called" "[ -f results.txt ]"
+    expected=$(readData 1 $TASKID 4)
     # calculate md5 hash of results file
-    md5_results=($($MD5_SHELL results.*))
+    md5_results=($($MD5_SHELL results.txt))
     # remove trailing filename and leave MD5 hashsum only:
     actual=$(echo $md5_results)
-    # declare a list of valid md5 hashes
-    declare -a expected=(49713444858274e0dcd3b4f51bb9513d 711dd6705a7f353e72afef285602ff26 72246e71e555278bbb8f59f906311489 586eb7421087bbd321d6637522afc0f2 2123fffa1d2cda5fdee16e1e1606ae44 177cf6611b35df84b7dbc90a6fc22336 0f554502d9d039e1e1ed247f55f7e320 e386a7601b776f1ec55a2f357cc63608 b846aba317f08a6d10be9860b0fd522f e957402ea82b4626a67c02e9482e6924 aa502da2f56bbc3822e5e11f7546e4c0 9e96dfe3d4c2393c9632a6bc78ac30a6)
-    for i in {0..11}
-    do
-        nvar=$(($i+1))
-        if [ $actual == ${expected[$i]} ]; then
-            echo "Solution for task "$nvar" was found and it seems to be correct."
-            assertTrue ${SHUNIT_TRUE}
-            return
-        fi
-    done
-    # report failure
-    fail "No correct solutions were found."
+    echo "MD5 is "$actual
+    # assertNotNull "Did you forget to set the VAR_2 variable?" $actual
+    assertEquals "File 'results.txt' has wrong contents!" $expected $actual
 }
 
+
+setUp_case1()
+{
+    cp dns-tunneling.log.bak dns-tunneling.log
+}
+
+setUp_case2()
+{
+    awk '(NR>15 &&NR <30)||(NR>1290 && NR<1305)||(NR>2210 && NR<2215)||(NR>15200 && NR<15380)||(NR>57230 && NR<57310)||(NR>146800 && NR<147000)||(NR>220142&&NR<220313)||(NR>172360 && NR<172560)' dns-tunneling.log.bak > dns-tunneling.log
+}
+
+test_VAR_1_case2()
+{
+    setUp_case2
+    source lab1.sh
+    expected=$(readData 2 $TASKID 2)
+    echo "VAR_1 is "$VAR_1
+    assertNotEquals "Did you forget to set the VAR_1 variable?" -1 $VAR_1
+    assertEquals "Wrong value of VAR_1!" $expected $VAR_1
+}
+
+test_VAR_2_case2()
+{
+    setUp_case2
+    source lab1.sh
+    expected=$(readData 2 $TASKID 3)
+    echo "VAR_2 is "$VAR_2
+    assertNotEquals "Did you forget to set the VAR_2 variable?" -1 $VAR_2
+    assertEquals "Wrong value of VAR_2!" $expected $VAR_2
+}
+
+test_results_file_case2()
+{
+    setUp_case2
+    source lab1.sh
+    touch results.txt
+    # assertTrue "script was called" "[ -f results.txt ]"
+    expected=$(readData 2 $TASKID 4)
+    # calculate md5 hash of results file
+    md5_results=($($MD5_SHELL results.txt))
+    # remove trailing filename and leave MD5 hashsum only:
+    actual=$(echo $md5_results)
+    echo "MD5 is "$actual
+    # assertNotNull "Did you forget to set the VAR_2 variable?" $actual
+    assertEquals "File 'results.txt' has wrong contents!" $expected $actual
+}
+
+
 # load shunit2
-. shunit2-2.1.6/src/shunit2
+. shunit2-2.1.7/shunit2
+
